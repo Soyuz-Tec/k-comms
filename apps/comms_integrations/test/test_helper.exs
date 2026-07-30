d1d7463fcb25db1eb4c6bd03ac7f88608b9551ba
Soyuz-Tec <web.soyuztec@gmail.com>
2026-07-29 21:01:36 -0400

    fix(test): ignore split integration support modules

--- apps/comms_integrations/test/test_helper.exs
+++ apps/comms_integrations/test/test_helper.exs
@@ -1,3 +1,3 @@
 ExUnit.start()
-Code.require_file("support/local_release_guard_options.exs", __DIR__)
-Code.require_file("support/object_storage_test_support.exs", __DIR__)
+Code.require_file("support/local_release_guard_options.ex", __DIR__)
+Code.require_file("support/object_storage_test_support.ex", __DIR__)
